2565bc50b4b98bb9f4f736d4da5b12156ae0c501
Alan_Yoon <shwan.yoon@hotmail.com>
2023-01-30 11:32:56 +0900

    Update 0l_environment.sh

--- 0l_environment.sh
+++ 0l_environment.sh
@@ -7,19 +7,24 @@ echo ""
 echo "================================"
 echo ""
 cd ~
-echo "Installing dependencies.."
+echo -e "\e[1m\e[32m1. Installing dependencies.. \e[0m"
 echo "===================="
 sleep 2
 apt install -y git vim zip unzip jq build-essential cmake clang llvm libgmp-dev secure-delete pkg-config libssl-dev lld tmux
 echo ""
-echo "Installing required packages.."
+echo -e "\e[1m\e[32m2. Installing required packages.. \e[0m"
 echo "===================="
 sleep 2
 curl -sL https://raw.githubusercontent.com/OLSF/libra/main/ol/util/setup.sh | bash
 echo ""
+echo ""
+echo ""
+echo ""
 echo "All required linux environment packages for 0l are already installed."
 echo ""
-echo "Preparing onboard condition now.. This script takes 30min more entirely until completed, so be patient, please."
+echo "Preparing onboard condition now.. "
+echo -e "\e[1m\e[32m3. Preparing onboard condition now.. \e[0m"
+echo "This script takes 30min more entirely until completed, so be patient, please."
 echo "===================="
 sudo useradd node -m -s /bin/bash
 cp ./0l_tmux.sh /home/node
