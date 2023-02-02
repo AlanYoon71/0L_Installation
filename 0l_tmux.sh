a8a600df9f846fdf71e09c3434c46811b151ded0
Alan_Yoon <shwan.yoon@hotmail.com>
2023-02-02 16:16:33 +0900

    Update 0l_tmux.sh

--- 0l_tmux.sh
+++ 0l_tmux.sh
@@ -78,7 +78,7 @@ do
                 tmux rename-window -t $session:$window 'waypoint'
                 sleep 1
 
-                tmux send-keys -t $session:$window '/home/node/bin/ol --config /home/node/.0L/0L.toml query --epoch > /home/node/bin/waypoint.txt && STR=$(cat /home/node/bin/waypoint.txt) && echo "${STR:(-73)}" > /home/node/bin/waypoint.txt && WAY=$(cat /home/node/bin/waypoint.txt) && echo ${#WAY} > /home/node/bin/waylength.txt' C-m
+                tmux send-keys -t $session:$window '/home/node/bin/ol --config /home/node/.0L/0L.toml query --epoch > /home/node/bin/waypoint.txt && STR=$(cat /home/node/bin/waypoint.txt) && echo "${STR:(-73)}" > /home/node/bin/waypoint.txt && WAY=$(cat /home/node/bin/waypoint.txt) && echo ${#WAY} > /home/node/bin/waylength.txt && sleep 2 && waylength=$(cat /home/node/bin/waylength.txt)' C-m
                 sleep 10
 
                 echo ""
