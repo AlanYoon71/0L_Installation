ae2d171b057c4b12fb47bdb8bcc3400f72bbb84c
Alan_Yoon <shwan.yoon@hotmail.com>
2023-02-03 01:33:06 +0900

    Update 0l_env.sh

--- 0l_env.sh
+++ 0l_env.sh
@@ -38,6 +38,17 @@ cp ./0l_tmux.sh /home/node
 chmod +x /home/node/0l_tmux.sh
 chmod go+rw /home/node/0l_tmux.sh
 sudo -u node /home/node/0l_tmux.sh &&
+CUR_DATE=`date +%Y%m%d` &&
+mkdir -p /root/0l_config_backup/"$CUR_DATE" &> /dev/null ;
+\cp /home/node/.0L/0L.toml /root/0l_config_backup/$CUR_DATE &> /dev/null ;
+\cp /home/node/.0L/account.json /root/0l_config_backup/$CUR_DATE &> /dev/null ;
+\cp /home/node/.0L/key_store.json /root/0l_config_backup/$CUR_DATE &> /dev/null ;
+\cp /home/node/.0L/vdf_proofs/proof_0.json /root/0l_config_backup/$CUR_DATE &> /dev/null ;
+sleep 1
+echo ""
+rm /home/node/bin/keygen.txt &> /dev/null ; rm /home/node/bin/waylength.txt &> /dev/null ; rm /home/node/bin/waypoint.txt &> /dev/null ; rm /home/node/bin/WAYPOINT.txt &> /dev/null ; rm /home/node/bin/update_check.txt &> /dev/null ;
+sleep 2
+echo -e "Your config files were saved into \e[1m\e[32m[ /root/0l_config_backup/$CUR_DATE ] \e[0mdirectory. There's no mnemonic info."
 echo ""
 echo ""
 echo "\e[1m\e[32mDone! \e[0m"
