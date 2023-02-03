c9aca32da8ed515dd6f9b94d921d4dc3d125eb00
Alan_Yoon <shwan.yoon@hotmail.com>
2023-02-03 16:16:13 +0900

    Update 0l_env.sh

--- 0l_env.sh
+++ 0l_env.sh
@@ -16,11 +16,13 @@ userdel node &> /dev/null ; rm -rf /home/node &> /dev/null ;
 sleep 3
 echo -e "\e[1m\e[32m1. Installing dependencies.. \e[0m"
 echo "===================="
+echo ""
 sleep 2
 apt install -y git vim zip unzip bc jq build-essential cmake clang llvm libgmp-dev secure-delete pkg-config libssl-dev lld tmux
 echo ""
 echo -e "\e[1m\e[32m2. Installing required linux packages.. \e[0m"
 echo "===================="
+echo ""
 sleep 2
 curl -sL https://raw.githubusercontent.com/OLSF/libra/main/ol/util/setup.sh | bash
 echo ""
@@ -33,16 +35,16 @@ echo ""
 echo "This script includes genesis mining and tower, so it takes 1 hour more entirely until all processes completed, so be patient, please."
 echo ""
 sudo useradd node -m -s /bin/bash
-cp ./0l_tmux.sh /home/node
+\cp -f ./0l_tmux.sh /home/node
 chmod +x /home/node/0l_tmux.sh
 chmod go+rw /home/node/0l_tmux.sh
 sudo -u node /home/node/0l_tmux.sh &&
 CUR_DATE=`date +%Y%m%d` &&
 mkdir -p /root/0l_config_backup/"$CUR_DATE" &> /dev/null ;
-\cp /home/node/.0L/0L.toml /root/0l_config_backup/$CUR_DATE &> /dev/null ;
-\cp /home/node/.0L/account.json /root/0l_config_backup/$CUR_DATE &> /dev/null ;
-\cp /home/node/.0L/key_store.json /root/0l_config_backup/$CUR_DATE &> /dev/null ;
-\cp /home/node/.0L/vdf_proofs/proof_0.json /root/0l_config_backup/$CUR_DATE &> /dev/null ;
+\cp -f /home/node/.0L/0L.toml /root/0l_config_backup/$CUR_DATE &> /dev/null ;
+\cp -f /home/node/.0L/account.json /root/0l_config_backup/$CUR_DATE &> /dev/null ;
+\cp -f /home/node/.0L/key_store.json /root/0l_config_backup/$CUR_DATE &> /dev/null ;
+\cp -f /home/node/.0L/vdf_proofs/proof_0.json /root/0l_config_backup/$CUR_DATE &> /dev/null ;
 sleep 1
 echo ""
 rm /home/node/bin/keygen.txt &> /dev/null ; rm /home/node/bin/waylength.txt &> /dev/null ; rm /home/node/bin/waypoint.txt &> /dev/null ; rm /home/node/bin/WAYPOINT.txt &> /dev/null ; rm /home/node/bin/update_check.txt &> /dev/null ;
