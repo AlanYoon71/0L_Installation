7ae5ede26a8a05d8e7d8653b908b5ffa79cbefee
Alan_Yoon <shwan.yoon@hotmail.com>
2023-02-09 18:03:02 +0900

    Update 0l_restart.sh

--- 0l_restart.sh
+++ 0l_restart.sh
@@ -21,23 +21,49 @@ K=10
 while [ $J -lt $K ]
 do
     sleep 60
-    HOUR=$(date "+%H")
-    MIN=$(date "+%M")
-    if [ $MIN == 19 ]
+    HOUR=$(date "+%H") &&
+    MIN=$(date "+%M") &&
+    if [ $MIN == 18 ]
     then
-        if [ $MIN -lt 20 ]
-        then
-            echo "syn1=$(curl 127.0.0.1:9101/metrics 2> /dev/null | grep diem_state_sync_version{type=\"highest\")" | at $HOUR:20 && export syn20=$(echo $syn1 | grep -o '[0-9]*') &&
-            if [ -z $syn20 ] ; then syn20=0 ; fi
-            sleep 1730
-        fi
+        echo "syn1=$(curl 127.0.0.1:9101/metrics 2> /dev/null | grep diem_state_sync_version{type=\"highest\")" | at $HOUR:20 && 
+        F=1
+        FF=10
+        while [ $F -lt $FF ]
+        do
+            sleep 3
+            MIN=$(date "+%M") &&
+            if [ $MIN == 20 ]
+            then
+                echo ""
+                export syn20=$(echo $syn1 | grep -o '[0-9]*') &&
+                echo "Current height : $syn20"
+                echo ""
+                F=15
+            fi
+        done
+        if [ -z $syn20 ] ; then syn20=0 ; fi
+        sleep 1760
     else
-        if [ $MIN == 49 ]
+        if [ $MIN == 48 ]
         then
-            echo "syn2=$(curl 127.0.0.1:9101/metrics 2> /dev/null | grep diem_state_sync_version{type=\"highest\")" | at $HOUR:50 && export syn50=$(echo $syn2 | grep -o '[0-9]*') &&
+            echo "syn2=$(curl 127.0.0.1:9101/metrics 2> /dev/null | grep diem_state_sync_version{type=\"highest\")" | at $HOUR:50 &&
+            G=1
+            GG=10
+            while [ $G -lt $GG ]
+            do
+                sleep 3
+                MIN=$(date "+%M") &&
+                if [ $MIN == 50 ]
+                then
+                    echo ""
+                    export syn50=$(echo $syn2 | grep -o '[0-9]*') &&
+                    echo "Current height : $syn50"
+                    echo ""
+                    G=15
+                fi
+            done
             if [ -z $syn20 ] ; then syn20=0 ; fi
             if [ -z $syn50 ] ; then syn50=0 ; fi
-            sleep 530
             if [ $syn50 == $syn20 ]
             then
                 echo ">>> Block height stuck at $syn50 !! <<<"
@@ -45,31 +71,46 @@ do
                 echo "Your validator will be restarted on the hour."
                 echo ""
                 echo ""
+                sleep 500
                 echo "/usr/bin/killall diem-node" | at $HOUR:59 &&
-                UP=$(expr $HOUR + 1)
-                sleep 55
+                P=1
+                PP=10
+                while [ $P -lt $PP ]
+                do
+                    sleep 3
+                    MIN=$(date "+%M") &&
+                    if [ $MIN == 59 ]
+                    then
+                        sleep 3
+                        echo ""
+                        pgrep diem-node || echo "Validator killed" &&
+                        echo ""
+                        P=15
+                    fi
+                done
+                UP=$(expr $HOUR + 1) &&
                 if [ $UP -gt 22 ]
                 then
                     UP=0
                 fi
                 echo "~/bin/diem-node --config ~/.0L/fullnode.node.yaml >> ~/.0L/logs/node.log 2>&1" | at $UP:00 &&
-                MIN=$(date "+%M")
                 R=1
                 RR=10
                 while [ $R -lt $RR ]
                 do
+                    sleep 3
+                    MIN=$(date "+%M")
                     if [ $MIN == 0 ]
                     then
-                        echo -e "================= \e[1m\e[33mRestarted!! \e[0m================="
                         echo "Network block height stuck at $syn50"
                         date '+%Y/%m/%d %I:%M %p UTC' --utc
                         echo -e "================= \e[1m\e[33mRestarted!! \e[0m================="
                         R=15
-                        sleep 1130
+                        sleep 1080
                     fi
                 done
             else
-                echo "Good. Block height is increasing now. $syn20 >>> $syn50"
+                echo "Block height is increasing now. $syn20 >>> $syn50"
                 echo ""
                 echo ""
             fi
