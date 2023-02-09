fd8cbdc29ca992f66d63907ea04247832183df83
Alan_Yoon <shwan.yoon@hotmail.com>
2023-02-09 14:14:36 +0900

    Update 0l_restart.sh

--- 0l_restart.sh
+++ 0l_restart.sh
@@ -29,34 +29,31 @@ do
     else
         if [ $MIN == 49 ]
         then
-            if [ $MIN -lt 50 ]
+            echo "syn2=$(curl 127.0.0.1:9101/metrics 2> /dev/null | grep diem_state_sync_version{type=\"highest\")" | at $HOUR:50 && export syn50=$(echo $syn2 | grep -o '[0-9]*') &&
+            if [ -z $syn20 ] ; then syn20=0 ; fi
+            if [ -z $syn50 ] ; then syn50=0 ; fi
+            sleep 530
+            if [ $syn50 == $syn20 ]
             then
-                echo "syn2=$(curl 127.0.0.1:9101/metrics 2> /dev/null | grep diem_state_sync_version{type=\"highest\")" | at $HOUR:50 && export syn50=$(echo $syn2 | grep -o '[0-9]*') &&
-                if [ -z $syn20 ] ; then syn20=0 ; fi
-                if [ -z $syn50 ] ; then syn50=0 ; fi
-                sleep 535
-                if [ $syn50 == $syn20 ]
+                echo "/usr/bin/killall diem-node" | at $HOUR:59 &&
+                UP=$(expr $HOUR + 1)
+                sleep 55
+                if [ $UP -gt 22 ]
                 then
-                    echo "/usr/bin/killall diem-node" | at $HOUR:59 &> /dev/null ;
-                    UP=$(expr $HOUR + 1)
-                    sleep 55
-                    if [ $UP -gt 22 ]
-                    then
-                        UP=0
-                    fi
-                    pgrep diem-node || echo "~/bin/diem-node --config ~/.0L/fullnode.node.yaml 2>&1 | multilog s50000000 n10 ~/.0L/logs/node" | at $UP:00 &&
-                    MIN=$(date "+%M")
-                    if [ $MIN == 0 ]
-                    then
-                        echo -e "================= \e[1m\e[33mRestarted!! \e[0m================="
-                        echo "Network block height stuck at $syn50"
-                        date
-                        echo -e "================= \e[1m\e[33mRestarted!! \e[0m================="
-                        sleep 1130
-                    fi
-                else
-                    echo "Good. Block height is increasing now. $syn20 >>> $syn50"
+                    UP=0
                 fi
+                echo "~/bin/diem-node --config ~/.0L/fullnode.node.yaml 2>&1 | multilog s50000000 n10 ~/.0L/logs/node" | at $UP:00 &&
+                MIN=$(date "+%M")
+                if [ $MIN == 0 ]
+                then
+                    echo -e "================= \e[1m\e[33mRestarted!! \e[0m================="
+                    echo "Network block height stuck at $syn50"
+                    date '+%Y/%m/%d %I:%M %p UTC' --utc
+                    echo -e "================= \e[1m\e[33mRestarted!! \e[0m================="
+                    sleep 1130
+                fi
+            else
+                echo "Good. Block height is increasing now. $syn20 >>> $syn50"
             fi
         fi
     fi
