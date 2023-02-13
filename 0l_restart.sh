227d21bf87d60078b54e75524ff7bce3ce0f3bae
Alan_Yoon <shwan.yoon@hotmail.com>
2023-02-13 13:53:54 +0900

    Update 0l_restart.sh

--- 0l_restart.sh
+++ 0l_restart.sh
@@ -118,11 +118,11 @@ do
                     export TIME=`date +%Y-%m-%dT%I:%M:%S`
                     if [ -z $syn11 ]
                     then
-                        echo "$TIME [INFO] Insufficient comparison data. Need to wait until next checkpoint."
+                        echo "$TIME [INFO] Insufficient comparison data for calculating TPS. Need to wait until next checkpoint."
                     else
                         if [ -z $syn1 ]
                         then
-                            echo "$TIME [INFO] Insufficient comparison data. Need to wait until next checkpoint."
+                            echo "$TIME [INFO] Insufficient comparison data for calculating TPS. Need to wait until next checkpoint."
                         else
                             NDIFF=`expr $syn2 - $syn1`
                             LDIFF=`expr $syn22 - $syn11`
