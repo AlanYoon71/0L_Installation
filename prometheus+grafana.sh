07e0df032e27794dff5b6b726c0eca6889417634
Alan_Yoon <shwan.yoon@hotmail.com>
2023-02-16 02:28:35 +0900

    Create prometheus+grafana.sh

---
+++ prometheus+grafana.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo ""
+echo "==============================";
+echo ""
+echo "Created by  //-\ ][_ //-\ ][\[";
+echo ""
+echo "==============================";
+echo "                    2023-02-17"
+echo ""
+echo "This script was created and checked only on 20.04 ubuntu environment."
+echo ""
+echo ""
