debc5f6796c752a60ccf7c4e8a1accde7a17c5e4
Alan_Yoon <shwan.yoon@hotmail.com>
2023-02-20 02:57:55 +0900

    Update 0l_restart.sh

--- 0l_restart.sh
+++ 0l_restart.sh
@@ -178,10 +178,27 @@ do
                             SPEED=$(echo "scale=2; $NTPS - $LTPS" | bc)
                             if [ $SPEED == 0 ]
                             then
-                                echo -e "$TIME [INFO] TPS >> Network : $NTPS[tx/s], Local : $LTPS[tx/s]"
+                                echo "$TIME [INFO] TPS >> Network : $NTPS[tx/s], Local : $LTPS[tx/s]"
+                                SEEK1=`tail -4 tower.log |grep "Success: Proof committed to chain"`
+                                if [ -z $SEEK1 ]
+                                then
+                                    echo -e "$TIME [WARN] \e[1m\e[35mIt looks like tower hasn't submitted a proof to the chain yet!\e[0m"
+                                else
+                                    SEEK2=`tail -2 tower.log | sed -n 1p | grep -o '[0-9]*'`
+                                    if [ -z $SEEK3 ] ; then SEEK3=0 ; fi
+                                    CHECKTOWER=`expr $SEEK3 - $SEEK2`
+                                    if [ $CHECKTOWER -gt 0 ]
+                                    then
+                                        echo -e "$TIME [INFO] Tower is mining normally. \e[1m\e[32mProof # $SEEK2 \e[0m"
+                                    fi
+                                fi
                             else
-                                CATCH=$(echo "scale=2; ( $LAG / $SPEED ) / 3600" | bc)
-                                echo "$TIME [INFO] Catchup Time >> $CATCH[Hr]"
+                                echo "$TIME [INFO] TPS >> Network : $NTPS[tx/s], Local : $LTPS[tx/s]"
+                                if [ $LAG -lt 0 ]
+                                then
+                                    CATCH=$(echo "scale=2; ( $LAG / $SPEED ) / 3600" | bc)
+                                    echo "$TIME [INFO] Catchup Time >> $CATCH[Hr]"
+                                fi
                             fi
                         fi
                     fi
@@ -248,6 +265,14 @@ do
                     ACTION4=00
                     if [ $MIN == $ACTION4 ]
                     then
+                        SEEK1=`tail -4 tower.log |grep "Success: Proof committed to chain"`
+                        if [ -z $SEEK1 ]
+                        then
+                            SEEK3=0
+                        else
+                            SEEK3=`tail -2 tower.log | sed -n 1p | grep -o '[0-9]*'`
+                            if [ -z $SEEK3 ]; then SEEK3=0 ; fi
+                        fi
                         export LL=`pgrep diem-node`
                         if [ -z $LL ]
                         then
