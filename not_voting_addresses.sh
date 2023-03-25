1b45df43527cd9ae906d20b263b0da08d5171dbc
Alan_Yoon <shwan.yoon@hotmail.com>
2023-03-26 06:41:17 +0900

    Update not_voting_addresses.sh

--- not_voting_addresses.sh
+++ not_voting_addresses.sh
@@ -17,7 +17,7 @@ then
     export TIME=`date +%Y-%m-%dT%H:%M:%S`
     echo -e "$TIME [INFO] All addresses in active set are voting now. Great!"
 else
-    echo -e "Current ConsensusDirectSend_Message \e[1m\e[31mUnresponsive \e[0mAddresses"
+    echo -e "Current ConsensusDirectSend_Message \e[1m\e[31mUnresponsive(Not voting) \e[0mAddresses"
     echo -e "\e[1m\e[31m========\e[0m"
     echo "$notvoting" | grep -Po 'Peer [^,]+' | cut -d' ' -f2 | sort -u
     echo -e "\e[1m\e[31m========\e[0m"
