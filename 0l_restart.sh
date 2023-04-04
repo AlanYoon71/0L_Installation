b114ae4f0628b3015b025b432c4185e6664520ac
Alan_Yoon <shwan.yoon@hotmail.com>
2023-04-04 12:37:12 +0900

    Update 0l_restart.sh

--- 0l_restart.sh
+++ 0l_restart.sh
@@ -450,7 +450,7 @@ do
             export TIME=`date +%Y-%m-%dT%H:%M:%S`
             if [ -z "$CONVERT" ]
             then
-                echo "$TIME [INFO] ========= Validator is running well now. ========="
+                echo "$TIME [INFO] Validator itself continues to operate without problems."
             fi
         fi
         sleep 1179
