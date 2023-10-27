0fd77a05e286331b723c298d334781ac208b47ed
Alan_Yoon <shwan.yoon@hotmail.com>
2023-10-27 22:43:55 +0900

    Update 0l_env_val.sh

--- 0l_env_val.sh
+++ 0l_env_val.sh
@@ -33,7 +33,7 @@ echo -e "\e[1m\e[32m1. Installing dependencies.. \e[0m"
 echo "===================="
 echo ""
 sleep 2
-sudo apt install -y git vim zip unzip daemontools bc jq build-essential cmake clang llvm libgmp-dev secure-delete pkg-config libssl-dev lld tmux
+sudo apt install -y sudo curl git vim mpstat zip unzip daemontools bc jq build-essential cmake clang llvm libgmp-dev secure-delete pkg-config libssl-dev lld tmux
 echo ""
 echo -e "\e[1m\e[32m2. Installing required linux packages.. \e[0m"
 echo "===================="
