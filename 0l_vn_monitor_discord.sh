18f33099cb53cd2d403a024173cbe039db5a0ddd
Alan_Yoon <shwan.yoon@hotmail.com>
2024-01-10 05:42:02 +0900

    Update 0l_vn_monitor_discord.sh

--- 0l_vn_monitor_discord.sh
+++ 0l_vn_monitor_discord.sh
@@ -233,7 +233,7 @@ while true; do
           else
             PIDCHECK=$(pgrep libra)
             RUNTIME=$(ps -p $PIDCHECK -o etime | awk 'NR==2')
-            message="\`\`\`diff\n+ ======= [ VALIDATOR ] ======== +   $SET nodes in the set, with $ACTIVE currently active.$vn_runtime\n\`\`\`"
+            message="\`\`\`diff\n+ ======= [ VALIDATOR ] ======== +   $SET nodes in the set, $ACTIVE active.$vn_runtime\n\`\`\`"
             send_discord_message "$message"
             message="\`\`\`arm\nEpoch jumped. $EPOCH1 ---> $EPOCH2  Vouches : $VOUCH\n\`\`\`"
             send_discord_message "$message"
@@ -244,7 +244,7 @@ while true; do
       else
         PIDCHECK=$(pgrep libra)
         RUNTIME=$(ps -p $PIDCHECK -o etime | awk 'NR==2')
-        message="\`\`\`diff\n+ ======= [ VALIDATOR ] ======== +   $SET nodes in the set, with $ACTIVE currently active.$vn_runtime\n\`\`\`"
+        message="\`\`\`diff\n+ ======= [ VALIDATOR ] ======== +   $SET nodes in the set, $ACTIVE active.$vn_runtime\n\`\`\`"
         send_discord_message "$message"
         if [[ $EPOCH1 -eq $EPOCH2 ]]
         then
@@ -314,7 +314,7 @@ while true; do
         SET=`curl 127.0.0.1:9101/metrics 2> /dev/null | grep diem_all_validators_voting_power{peer_id= | wc -l`
         PIDCHECK=$(pgrep libra)
         RUNTIME=$(ps -p $PIDCHECK -o etime | awk 'NR==2')
-        message="\`\`\`diff\n+ ======= [ VALIDATOR ] ======== +   $SET nodes in the set, with $ACTIVE currently active.$vn_runtime\n\`\`\`"
+        message="\`\`\`diff\n+ ======= [ VALIDATOR ] ======== +   $SET nodes in the set, $ACTIVE active.$vn_runtime\n\`\`\`"
         send_discord_message "$message"
         message="\`\`\`arm\nTotal    balance : $BALANCET1 ---> $BALANCET2  $BALANCETDIFF\n\`\`\`"
         send_discord_message "$message"
@@ -348,7 +348,7 @@ while true; do
             minutes=$(printf "%02d" $minutes)
             vn_runtime="  VN uptime : ${days}d ${hours}h ${minutes}m"
           fi
-          message="\`\`\`diff\n+ ======= [ VALIDATOR ] ======== +   $SET nodes in the set, with $ACTIVE currently active.$vn_runtime\n\`\`\`"
+          message="\`\`\`diff\n+ ======= [ VALIDATOR ] ======== +   $SET nodes in the set, $ACTIVE active.$vn_runtime\n\`\`\`"
           send_discord_message "$message"
           message="\`\`\`arm\nProposal : +$PROPDIFF > $PROP2  Synced version : +$SYNCDIFF > $SYNC2  Block height : +$HEIGHTDIFF > $HEIGHT2\n\`\`\`"
           send_discord_message "$message"
