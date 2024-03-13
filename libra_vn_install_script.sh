a276f99e10c3280fef957dc7ab3aa6bdb1e206c5
Alan_Yoon <shwan.yoon@hotmail.com>
2024-03-14 08:25:30 +0900

    Update libra_vn_install_script.sh

--- libra_vn_install_script.sh
+++ libra_vn_install_script.sh
@@ -27,20 +27,18 @@ echo ""
 echo -e "\e[1m\e[32m2. Download libra framework source and git pull.\e[0m"
 echo ""
 sleep 3
-if [ -d ~/libra-framework ]; then
-    cd ~/libra-framework
-    bash ./util/dev_setup.sh -t && source ~/.bashrc
-    sudo rm /usr/lib/python3.*/EXTERNALLY-MANAGED &> /dev/null;
-    bash ./util/dev_setup.sh -t && source ~/.bashrc
-    cd ~/libra-framework && git fetch && git pull
-else
-    git clone https://github.com/0LNetworkCommunity/libra-framework && cd ~/libra-framework
-    bash ./util/dev_setup.sh -t && source ~/.bashrc
-    sudo rm /usr/lib/python3.*/EXTERNALLY-MANAGED &> /dev/null;
-    bash ./util/dev_setup.sh -t && source ~/.bashrc
-    cd ~/libra-framework && git fetch && git pull
-fi
-sudo rm /usr/lib/python3.*/EXTERNALLY-MANAGED &> /dev/null;
+# if [ -d ~/libra-framework ]; then
+#     cd ~/libra-framework
+#     bash ./util/dev_setup.sh -t && source ~/.bashrc
+#     cd ~/libra-framework && git fetch && git pull
+# else
+#     git clone https://github.com/0LNetworkCommunity/libra-framework && cd ~/libra-framework
+#     bash ./util/dev_setup.sh -t && source ~/.bashrc
+#     cd ~/libra-framework && git fetch && git pull
+# fi
+git clone https://github.com/0LNetworkCommunity/libra-framework && cd ~/libra-framework
+bash ./util/dev_setup.sh -t && source ~/.bashrc
+cd ~/libra-framework && git fetch && git pull
 echo ""
 echo -e "\e[1m\e[32m3. Build libra binary files.\e[0m"
 echo ""
@@ -97,10 +95,6 @@ full_node_networks:
   seed_addrs:
     cb7e573123b67b0bb957d23f0d11c65b0b5438815b3750461c3815d507fb5649:
     - "/ip4/73.181.115.53/tcp/6182/noise-ik/0xcb7e573123b67b0bb957d23f0d11c65b0b5438815b3750461c3815d507fb5649/handshake/0"
-    1017ce1abc30e356660b8b0542275f2fb4373b5f8a82b7800a5b3fdf718ae55f:
-    - "/ip4/70.15.242.6/tcp/6182/noise-ik/0x1017ce1abc30e356660b8b0542275f2fb4373b5f8a82b7800a5b3fdf718ae55f/handshake/0"
-    dcab287b256bb1e90cda2537553ee19cac195ce67c2fefc7ff25b8aaf2368e6d:
-    - "/ip4/222.101.31.242/tcp/6182/noise-ik/0xdcab287b256bb1e90cda2537553ee19cac195ce67c2fefc7ff25b8aaf2368e6d/handshake/0"
     619898b2f99fba7b25fae35e3eab03164d7d9ce0d10abe8f6ceae9a43ffa1c34:
     - "/ip4/65.109.80.179/tcp/6182/noise-ik/0x619898b2f99fba7b25fae35e3eab03164d7d9ce0d10abe8f6ceae9a43ffa1c34/handshake/0"
     bc2d1a55f90dfd27e4ef871285f13386997aecf609a3c4d4c4527efc9b2d193e:
@@ -109,8 +103,22 @@ full_node_networks:
     - "/ip4/144.76.104.93/tcp/6182/noise-ik/0x3b315502851df6d3004c69cf17714559d2407e28477b622d4e28c7b876859d0a/handshake/0"
     46b3705ebeeb469dd980210ace3462a91320249d37e90d1279a9a9df94995278:
     - "/ip4/136.243.59.175/tcp/6182/noise-ik/0x46b3705ebeeb469dd980210ace3462a91320249d37e90d1279a9a9df94995278/handshake/0"
+    1017ce1abc30e356660b8b0542275f2fb4373b5f8a82b7800a5b3fdf718ae55f:
+    - "/ip4/70.15.242.6/tcp/6182/noise-ik/0x1017ce1abc30e356660b8b0542275f2fb4373b5f8a82b7800a5b3fdf718ae55f/handshake/0"
     9ac157ee324e4129c9edac7ba5eca70af299929ae8c0d7362f4e6c75a7ac447e:
     - "/ip4/104.248.94.195/tcp/6182/noise-ik/0x9ac157ee324e4129c9edac7ba5eca70af299929ae8c0d7362f4e6c75a7ac447e/handshake/0"
+    15878523172483c401cc6a674d9a127586db107cf062b113661f6b8521fccf16:
+    - "/ip4/178.18.254.226/tcp/6182/noise-ik/0x15878523172483c401cc6a674d9a127586db107cf062b113661f6b8521fccf16/handshake/0"
+    3dbb2cc9a7c17072b18ed4aa31c806acfbc0ad92da934a2e1338a715e7473d48:
+    - "/ip4/73.181.115.53/tcp/6182/noise-ik/0x3dbb2cc9a7c17072b18ed4aa31c806acfbc0ad92da934a2e1338a715e7473d48/handshake/0"
+    febaed185e04d0f158998b39501796d0b79235399796e96a1c9a3fac2f31ab57:
+    - "/ip4/38.242.137.192/tcp/6182/noise-ik/0xfebaed185e04d0f158998b39501796d0b79235399796e96a1c9a3fac2f31ab57/handshake/0"
+    9b07a7d587d5f515f4b77a2f751d0ca02e7a8cfb8f5550b26e5498d480cbd960:
+    - "/ip4/209.38.172.53/tcp/6182/noise-ik/0x9b07a7d587d5f515f4b77a2f751d0ca02e7a8cfb8f5550b26e5498d480cbd960/handshake/0"
+    6ead5e1c7fe9f7c09e965f230cf8cf3cbbcacc33a562737aebd7387a90b04a43:
+    - "/ip4/65.109.80.179/tcp/6182/noise-ik/0x6ead5e1c7fe9f7c09e965f230cf8cf3cbbcacc33a562737aebd7387a90b04a43/handshake/0"
+    dcab287b256bb1e90cda2537553ee19cac195ce67c2fefc7ff25b8aaf2368e6d:
+    - "/ip4/222.101.31.242/tcp/6182/noise-ik/0xdcab287b256bb1e90cda2537553ee19cac195ce67c2fefc7ff25b8aaf2368e6d/handshake/0"
     
 api:
   enabled: true
